b0da6cf4125e02df784849c68e54b2908c6425fc
MaksZiem <mtziembicki@gmail.com>
2024-04-03 13:08:17 +0200

    setting Counter Redux

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -2,25 +2,27 @@ import { useSelector, useDispatch } from 'react-redux';
 
 import classes from './Counter.module.css';
 
+import { counterActions } from '../store';
+
 const Counter = () => {
   const dispatch = useDispatch()
   const counter = useSelector(state => state.counter) //zawsze gdy wartosc counter sie zmeni w react store komponent bedzie renderowal ponownie
   const show = useSelector(state => state.showCounter)
 
   const increaseHandler = () => {
-    dispatch({type: 'increase', amount: 5})
+    dispatch(counterActions.increase(5))
   }
 
   const incrementHandler = () => {
-    dispatch({type: 'increment'})
+    dispatch(counterActions.increment())
   }
 
   const decrementHandler = () => {
-    dispatch({type: 'decrement'})
+    dispatch(counterActions.decrement())
   }
 
   const toggleCounterHandler = () => {
-    dispatch({type: 'toggle'})
+    dispatch(counterActions.toggle())
   };
 
   return (
